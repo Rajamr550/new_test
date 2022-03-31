29badcacd391c2b401c2ee9ed7c51ad4d78a9615
raja <raja.m@zensar.com>
2022-04-01 01:03:12 +0530

    new change

--- CityMockitoDemo/src/main/java/com/zensar/abc.java
+++ CityMockitoDemo/src/main/java/com/zensar/abc.java
@@ -4,7 +4,12 @@ public class abc {
 
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
-System.out.println("dn");
+	System.out.println("dn");
+	System.out.println("dn");
+	System.out.println("dn");
+	System.out.println("dn");
+	System.out.println("dn");
+	System.out.println("dn");
     }
 
 }
