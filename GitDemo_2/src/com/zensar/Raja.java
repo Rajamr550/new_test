9ec687213cabd3d5d96305280e8574b60d178c34
Rajamr550 <45516738+Rajamr550@users.noreply.github.com>
2022-03-31 21:41:17 +0530

    sixth

---
+++ GitDemo_2/src/com/zensar/Raja.java
@@ -0,0 +1,12 @@
+package com.zensar;
+
+public class Raja {
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	System.out.println("abcd");
+	System.out.println("efgh");
+
+    }
+
+}
